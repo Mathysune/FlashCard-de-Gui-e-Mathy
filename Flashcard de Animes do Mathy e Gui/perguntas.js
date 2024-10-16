e6650472fc148ba97a55c00d6d87fcdfc4d4ac6a
Mathysune <159062706+Mathysune@users.noreply.github.com>
2024-10-16 14:11:24 -0300

    Add files via upload

---
+++ Flashcard de Animes do Mathy e Gui/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Dragon Ball Z',
+    'Qual a única transformação do Kuririn?',
+    'A única transformação de Kuririn, que é da raça Humana é: Kaioken'
+)
+
+criaCartao(
+    'Naruto',
+    'Qual o nome do mestre do Naruto?',
+    'O mestre de Naruto, que o ensinou a lutar e ser um homem é: Jiraiya'
+)
+
+criaCartao(
+    'Shingeki no Kyojin',
+    'Qual o nome do maior titan do Mundo de Attack on Titan?',
+    'O maior titan do Mundo, que solta um gás quando se transforma é o titan Colossal'
+)
+
+criaCartao(
+    'Black Clover',
+    'Qual o nome do Rei Mago?',
+    'O nome do Rei Mago de Black Clover é Julius Novachrono.'
+)
+
+criaCartao(
+    'Nanatsu no Taizai',
+    'Qual o nome do antigo grupo que o Meliodas se renegou?',
+    'O antigo grupo de Meliodas se chamava 10 Mandamentos, onde ele portava o mandamento do amor.'
+)
+
+criaCartao(
+    'Jujutsu Kaisen',
+    'Como Itadori conquistou seu poder?',
+    'Ele conquistou suas habilidades quando consumiu as Pinturas da Morte número 4 a 9.'
+)
